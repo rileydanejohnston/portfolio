4fb82ad8c93b7fd60636255491dc52872311e6f7
Riley Johnston <rileydanejohnston@gmail.com>
2022-01-28 22:03:24 -0600

    refactor: move Sider into it's own component SideMenu

--- portfolio/src/components/App/App.js
+++ portfolio/src/components/App/App.js
@@ -1,61 +1,34 @@
-import React, { useState } from 'react';
+import React from 'react';
 import 'antd/dist/antd.css';
-import { 
-  HomeOutlined,
-  UserOutlined,
-  MailOutlined,
-  ToolOutlined
-} from '@ant-design/icons';
+import SideMenu from '../SideMenu/SideMenu';
 import {
   Layout,
-  Menu
 } from 'antd';
-import { Route, Switch, Link } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 const {
   Header,
   Content,
   Footer,
-  Sider
 } = Layout;
+
 
 
 function App() {
 
-  const [collapsed, setCollapsed] = useState(false);
-
   return (
     <Layout style={{ minHeight: '100vh' }}>
-      <Sider
-        collapsible
-        collapsed={collapsed}
-        onCollapse={() => setCollapsed(!collapsed)}
-      >
-        <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-          <Menu.Item style={{marginTop: 0}} key={1} icon={<HomeOutlined />}>
-            <Link to='/'>Home</Link>
-          </Menu.Item>
-          <Menu.Item key={2} icon={<UserOutlined />}>
-            <Link to='/about'>About</Link>
-          </Menu.Item>
-          <Menu.Item key={3} icon={<ToolOutlined />}>
-            <Link to='/projects'>Projects</Link>
-          </Menu.Item>
-          <Menu.Item key={4} icon={<MailOutlined />}>
-            <Link to='/contact'>Contact</Link>
-          </Menu.Item>
-        </Menu>
-      </Sider>
+      <SideMenu />
       <Layout>
-      <Header style={{background: '#fff'}}></Header>
-      <Content>
-        <Switch>
-          <Route exact path='/'></Route>
-          <Route path='/about'></Route>
-          <Route path='/projects'></Route>
-          <Route path='/contact'></Route>
-        </Switch>
-      </Content>
-      <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+        <Header style={{background: '#fff'}}></Header>
+        <Content>
+          <Switch>
+            <Route exact path='/'></Route>
+            <Route path='/about'></Route>
+            <Route path='/projects'></Route>
+            <Route path='/contact'></Route>
+          </Switch>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
       </Layout>
     </Layout>
   )
