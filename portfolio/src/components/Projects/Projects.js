d33a651d00b57c1aba07bcdeddf2955b071df413
Riley Johnston <rileydanejohnston@gmail.com>
2022-01-31 02:54:25 -0600

    fix: update project constants file name

--- portfolio/src/components/Projects/Projects.js
+++ portfolio/src/components/Projects/Projects.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Project from '../Project/Project';
 import { ProjectList, ProjectSection } from './styledProjects';
-import { projectData } from '../../constants/project-constants';
+import { projectData } from '../../constants/project';
 
 export default function Projects() {
 
