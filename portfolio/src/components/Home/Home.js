eacf31464c243028dc77132dbb8ee1f1aec2985c
Riley Johnston <rileydanejohnston@gmail.com>
2022-01-31 02:17:10 -0600

    fix: content of Home route

--- portfolio/src/components/Home/Home.js
+++ portfolio/src/components/Home/Home.js
@@ -7,9 +7,10 @@ export default function Home() {
   return (
     <HomeWrapper>
       <Space direction='vertical' size='medium'>
-        <Text>I'm interested in new ideas. I became a web developer to turn new ideas into a reality.</Text>
-        <Text>I like to use the MERN stack to build applications. I'm comfortable working on the frontend or the backend (although I think the backend is more fun).</Text>
-        <Text>I also love tinkering with new technologies. I'm learning how to use the Ant Design library, which I used to create this page. It's awesome!</Text>
+        <Text>I started programming by accident. I was working as a customer service agent at a software company. I took an online CS class to sound more knowledgeable when talking to customers. I thought the course would teach me about servers. The topic of the class was programming with C instead. I've completed a ten-month web development boot camp, gone back to college for computer science, deployed two full-stack projects, and created a portfolio in the two years since then.</Text>
+        <Text>I like to use the MERN stack to build applications. I'm comfortable working on the front end or the back end. I also love tinkering with new technologies. I'm learning how to use the Ant Design library, which I used to create this page. It's awesome!</Text>
+        <Text>I also like programming with C++. I recently built a farm application, which allowed users to create a farm and calculate financial figures. The app used many object-oriented principles like polymorphism, inheritance, and composition. It could also add animals and crops to the farm dynamically using pointers and vectors.</Text>
+        <Text>I became a web developer to turn exciting ideas into a reality. My goal is to solve problems, help people, protect the environment, and contribute to the world. </Text>
       </Space>
     </HomeWrapper>
   );
