b2f3fe21d9b66dcc983e625d8e84c1cdc1b76774
Riley Johnston <rileydanejohnston@gmail.com>
2022-01-31 02:57:52 -0600

    refactor: move About text into a constants file

--- portfolio/src/components/About/About.js
+++ portfolio/src/components/About/About.js
@@ -2,6 +2,13 @@ import React from 'react';
 import { Text } from '../Home/styledHome';
 import { AboutWrapper, AuthorImage, TextWrapper} from './styledAbout';
 import authImg from '../../images/riley-og.png';
+import { aboutData } from '../../constants/about';
+const {
+  places,
+  college,
+  movies,
+  bike
+} = aboutData;
 
 export default function About() {
   return (
@@ -11,10 +18,10 @@ export default function About() {
         alt='Riley Johnston, creator of this site'
       />
       <TextWrapper>
-        <Text>I grew up in Minnesota. I earned a Bachelor of Science in Business Administration from the University of South Carolina. After college, I moved to California, where I worked in the film industry as a camera assistant. I've lived in Austin, Texas, since 2021.</Text>
-        <Text> I started studying computer science at Glendale Community College in August 2020. I plan to continue my education at Oregon State University.</Text>
-        <Text>I like movies, books, shows, video games, and anything with a great story. My favorite movie is Aliens. The last book I read was Dune. I'm currently watching Ted Lasso.</Text>
-        <Text>I also like riding my bike. I bought a Trek hybrid when I moved to Texas, so I've been spending my time riding in the country. The best part about it is seeing the horses and cows. The worst part is getting chased by dogs.</Text>
+        <Text>{places}</Text>
+        <Text> {college}</Text>
+        <Text>{movies}</Text>
+        <Text>{bike}</Text>
       </TextWrapper>
     </AboutWrapper>
   );
---
+++ portfolio/src/constants/about.js
@@ -0,0 +1,6 @@
+export const aboutData = {
+  places: 'I grew up in Minnesota. I earned a Bachelor of Science in Business Administration from the University of South Carolina. After college, I moved to California, where I worked in the film industry as a camera assistant. I\'ve lived in Austin, Texas, since 2021.',
+  college: 'I started studying computer science at Glendale Community College in August 2020. I plan to continue my education at Oregon State University.',
+  movies: 'I like movies, books, shows, video games, and anything with a great story. My favorite movie is Aliens. The last book I read was Dune. I\'m currently watching Ted Lasso.',
+  bike: 'I also like riding my bike. I bought a Trek hybrid when I moved to Texas, so I\'ve been spending my time riding in the country. The best part about it is seeing the horses and cows. The worst part is getting chased by dogs.'
+};
