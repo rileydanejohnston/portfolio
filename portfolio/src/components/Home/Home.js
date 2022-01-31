d1ede907347719098928da44423ef05763d7ee62
Riley Johnston <rileydanejohnston@gmail.com>
2022-01-31 02:53:28 -0600

    refactor: move Home text to a constants file

--- portfolio/src/components/Home/Home.js
+++ portfolio/src/components/Home/Home.js
@@ -1,14 +1,21 @@
 import { Text } from './styledHome';
 import React from 'react';
 import { HomeWrapper } from './styledHome';
+import { homeData } from '../../constants/home';
+const {
+  beginning,
+  webPreferences,
+  farmApp,
+  goals
+} = homeData;
 
 export default function Home() {
   return (
     <HomeWrapper>
-      <Text>I started programming by accident. I was working as a customer service agent at a software company. I took an online CS class to sound more knowledgeable when talking to customers. I thought the course would teach me about servers. The topic of the class was programming with C instead. I've completed a ten-month web development boot camp, gone back to college for computer science, deployed two full-stack projects, and created a portfolio in the two years since then.</Text>
-      <Text>I like to use the MERN stack to build applications. I'm comfortable working on the front end or the back end. I also love tinkering with new technologies. I'm learning how to use the Ant Design library, which I used to create this page. It's awesome!</Text>
-      <Text>I also like programming with C++. I recently built a farm application, which allowed users to create a farm and calculate financial figures. The app used many object-oriented principles like polymorphism, inheritance, and composition. It could also add animals and crops to the farm dynamically using pointers and vectors.</Text>
-      <Text>I became a web developer to turn exciting ideas into a reality. My goal is to solve problems, help people, protect the environment, and contribute to the world. </Text>
+      <Text>{beginning}</Text>
+      <Text>{webPreferences}</Text>
+      <Text>{farmApp}</Text>
+      <Text>{goals}</Text>
     </HomeWrapper>
   );
 }
---
+++ portfolio/src/constants/home.js
@@ -0,0 +1,6 @@
+export const homeData = {
+  beginning: 'I started programming by accident. I was working as a customer service agent at a software company. I took an online CS class to sound more knowledgeable when talking to customers. I thought the course would teach me about servers. The topic of the class was programming with C instead. I\'ve completed a ten-month web development boot camp, gone back to college for computer science, deployed two full-stack projects, and created a portfolio in the two years since then.',
+  webPreferences: 'I like to use the MERN stack to build applications. I\'m comfortable working on the front end or the back end. I also love tinkering with new technologies. I\'m learning how to use the Ant Design library, which I used to create this page. It\'s awesome!',
+  farmApp: 'I also like programming with C++. I recently built a farm application, which allowed users to create a farm and calculate financial figures. The app used many object-oriented principles like polymorphism, inheritance, and composition. It could also add animals and crops to the farm dynamically using pointers and vectors.',
+  goals: 'I became a web developer to turn exciting ideas into a reality. My goal is to solve problems, help people, protect the environment, and contribute to the world.'
+};
