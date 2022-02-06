cb50f0937929a3752ce958145cd4263cb5d6c628
Riley Johnston <rileydanejohnston@gmail.com>
2022-02-06 01:40:47 -0600

    refactor: iterate through navData instead of hardcoding every nav item

---
+++ src/constants/navigation.js
@@ -0,0 +1,17 @@
+export const navData = [{
+  path: '/',
+  text: 'Home'
+},
+{
+  path: '/projects',
+  text: 'Projects'
+},
+{
+  path: '/about',
+  text: 'About'
+},
+{
+  path: '/contact',
+  text: 'Contact'
+}
+];
