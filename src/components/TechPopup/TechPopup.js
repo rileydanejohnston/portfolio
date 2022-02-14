ac460012959a0f18716d240f5dd5711aad10e2ee
Riley Johnston <rileydanejohnston@gmail.com>
2022-02-13 23:45:23 -0600

    refactor: create a constant file for technologies, import to TechPopup

--- src/components/TechPopup/TechPopup.js
+++ src/components/TechPopup/TechPopup.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Popup from '../Popup/Popup'
 import { Bullet, PopupContent, ListGroup, TechList, Title } from './styledTechPopup'
+import { main, other } from '../../constants/technologies'
 
 export default function TechPopup({ closePopups, techOpen }) {
   return (
@@ -12,30 +13,25 @@ export default function TechPopup({ closePopups, techOpen }) {
         <ListGroup>
           <Title>Main</Title>
           <TechList>
-            <Bullet>React</Bullet>
-            <Bullet>JavaScript (ES6)</Bullet>
-            <Bullet>Styled-Components</Bullet>
-            <Bullet>CSS3</Bullet>
-            <Bullet>HTML5</Bullet>
-            <Bullet>Node.js</Bullet>
-            <Bullet>Express.js</Bullet>
-            <Bullet>MongoDB</Bullet>
-            <Bullet>Jest</Bullet>
+            {
+              main.map((tech) => {
+                return (
+                  <Bullet>{tech}</Bullet>
+                )
+              })
+            }
           </TechList>
         </ListGroup>
         <ListGroup>
           <Title>Other</Title>
           <TechList>
-            <Bullet>SuperTest</Bullet>
-            <Bullet>Mongoose</Bullet>
-            <Bullet>Celebrate/Joi</Bullet>
-            <Bullet>JSON web tokens</Bullet>
-            <Bullet>Bcryptjs</Bullet>
-            <Bullet>React SizeMe</Bullet>
-            <Bullet>Particle.js</Bullet>
-            <Bullet>Git/Github</Bullet>
-            <Bullet>Google Cloud</Bullet>
-            <Bullet>NGINX</Bullet>
+          {
+              other.map((tech) => {
+                return (
+                  <Bullet>{tech}</Bullet>
+                )
+              })
+            }
           </TechList>
         </ListGroup>
       </PopupContent>
---
+++ src/constants/technologies.js
@@ -0,0 +1,22 @@
+export const main = [
+  'React',
+  'JavaScript (ES6)',
+  'Styled-Components',
+  'CSS3',
+  'HTML5',
+  'Node.js',
+  'Express.js',
+  'MongoDB',
+  'Jest'
+];
+
+export const other = [
+  'SuperTest',
+  'Mongoose',
+  'Celebrate/Joi',
+  'JSON web tokens',
+  'Bcryptjs',
+  'Git/Github',
+  'Google Cloud',
+  'NGINX'
+];
