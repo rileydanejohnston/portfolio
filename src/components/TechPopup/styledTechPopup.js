b2408d2f8b9b6d31b2e53df408a4af2874bd9a12
Riley Johnston <rileydanejohnston@gmail.com>
2022-02-13 23:26:05 -0600

    feat: add responsiveness to techpopup

--- src/components/TechPopup/styledTechPopup.js
+++ src/components/TechPopup/styledTechPopup.js
@@ -6,11 +6,20 @@ export const Title = styled.h3`
   width: 100%;
   border-bottom: 1px solid white;
   margin-bottom: 8px;
+
+  @media( max-width: 625px) {
+    font-size: 20px;
+  }
 `;
 
 export const PopupContent = styled.div`
   display: flex;
   grid-gap: 64px;
+
+  @media( max-width: 625px ) {
+    flex-direction: column;
+    grid-gap: 32px;
+  }
 `;
 
 export const ListGroup = styled.div`
@@ -22,4 +31,8 @@ export const TechList = styled.ul`
 
 export const Bullet = styled.li`
   font-size: 18px;
+
+  @media( max-width: 625px) {
+    font-size: 14px;
+  }
 `;
